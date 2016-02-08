625f29c388f89f927377a985b5ec08fc1f48de9a
Aaditya Sriram <aadisriram@gmail.com>
2016-02-07 20:08:28 -0500

    Base servlet for login and create user (empty)

---
+++ src/main/java/com/freshspire/api/login/CreateUserServlet.java
@@ -0,0 +1,20 @@
+package com.freshspire.api.login;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by aadisriram on 2/7/16.
+ */
+public class CreateUserServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
