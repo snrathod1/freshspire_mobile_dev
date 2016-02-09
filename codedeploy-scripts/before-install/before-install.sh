91bb6b3ff0a92489102570454868120dcd8a8664
mwreesjo <mwreesjo@ncsu.edu>
2016-02-09 16:03:37 -0500

    Add logging statements to before-install

--- codedeploy-scripts/before-install/before-install.sh
+++ codedeploy-scripts/before-install/before-install.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-exec 3>&1 4>&2
-trap 'exec 2>&4 1>&3' 0 1 2 3
-exec 1>log.out 2>&1
-### Everything below will go to the file 'log.out'
 
-
-echo "$(date) : running codedeploy beforeinstall script" >&3
+echo "Codedeploy script running" >> /home/ec2-user/log.out
+echo "About to stop tomcat8" >> /home/ec2-user/log.out
 sudo /etc/init.d/tomcat8 stop
 mvn package -f ../../pom.xml
 mv ../../target/rest-api-freshspire.war /var/lib/tomcat8/webapps
+
+echo ".war file hopefully moved, here's what's in the tomcat folder:" >> /home/ec2-user/log.out
+echo $(ls /var/lib/tomcat8/webapps) >> /home/ec2-user/log.out
