3db4ed242267b3f53e8df22015846e343863e3f3
mwreesjo <mwreesjo@ncsu.edu>
2016-02-09 13:21:54 -0500

    Update beforeinstall script to log output

--- codedeploy-scripts/before-install/before-install.sh
+++ codedeploy-scripts/before-install/before-install.sh
@@ -1,6 +1,11 @@
-#/bin/sh
+#!/bin/bash
+exec 3>&1 4>&2
+trap 'exec 2>&4 1>&3' 0 1 2 3
+exec 1>log.out 2>&1
+### Everything below will go to the file 'log.out'
 
-mvn package -f ../../pom.xml
+
+echo "$(date) : running codedeploy beforeinstall script" >&3
 sudo /etc/init.d/tomcat8 stop
+mvn package -f ../../pom.xml
 mv ../../target/rest-api-freshspire.war /var/lib/tomcat8/webapps
-sudo /etc/init.d/tomcat8 start
