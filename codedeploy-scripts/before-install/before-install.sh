b67ea2ba6fadc0ba04724f6521f8e183d6699b6a
mwreesjo <mwreesjo@ncsu.edu>
2016-02-09 16:10:30 -0500

    Change relative paths in before-install.sh

--- codedeploy-scripts/before-install/before-install.sh
+++ codedeploy-scripts/before-install/before-install.sh
@@ -5,7 +5,7 @@ echo "Codedeploy script running" >> /home/ec2-user/log.out
 echo "In the following directory: " $(pwd) >> /home/ec2-user/log.out
 echo "About to stop tomcat8" >> /home/ec2-user/log.out
 sudo /etc/init.d/tomcat8 stop
-mvn package -f ../../pom.xml
+mvn package -f pom.xml
 mv ../../target/rest-api-freshspire.war /var/lib/tomcat8/webapps
 
 echo ".war file hopefully moved, here's what's in the tomcat folder:" >> /home/ec2-user/log.out
