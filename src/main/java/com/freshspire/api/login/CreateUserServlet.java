aff0ee47f3d4910c645f18d05ecd8c85500c05bf
Aaditya Sriram <aadisriram@gmail.com>
2016-02-10 23:07:03 -0500

    Added hibernate and unirest (http client) and clean up

--- src/main/java/com/freshspire/api/login/CreateUserServlet.java
+++ src/main/java/com/freshspire/api/login/CreateUserServlet.java
@@ -1,14 +1,11 @@
 package com.freshspire.api.login;
 
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.Unirest;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.net.URLConnection;
 
 /**
  * Created by aadisriram on 2/7/16.
@@ -17,22 +14,12 @@ import java.net.URLConnection;
 @RequestMapping("/createuser")
 public class CreateUserServlet {
 
-    @RequestMapping(value = "/{authId}", method = RequestMethod.GET, produces = "application/json")
+    @RequestMapping(value = "/{login_type}/{authId}", method = RequestMethod.GET, produces = "application/json")
     public String hello(@PathVariable String authId) throws Exception {
-        URL fbAuth = new URL("https://graph.facebook.com/me?access_token=" + authId);
-        URLConnection urlConnection = fbAuth.openConnection();
-        BufferedReader in = new BufferedReader(
-                new InputStreamReader(
-                        urlConnection.getInputStream()));
-        StringBuilder inputLineBuffer = new StringBuilder();
-        String inputLine;
+        HttpResponse response = Unirest.get("https://graph.facebook.com/me")
+                            .queryString("access_token", authId)
+                            .asString();
 
-        while ((inputLine = in.readLine()) != null) {
-            inputLineBuffer.append(inputLine);
-        }
-
-        in.close();
-
-        return inputLineBuffer.toString();
+        return response.getBody().toString();
     }
 }
