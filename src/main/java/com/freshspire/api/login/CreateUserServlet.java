720be81466141736cc0c92f73cd8a4de8911f1d0
Aaditya Sriram <aadisriram@gmail.com>
2016-02-10 21:51:20 -0500

    Converted to Spring MVC service (no Views for us)

--- src/main/java/com/freshspire/api/login/CreateUserServlet.java
+++ src/main/java/com/freshspire/api/login/CreateUserServlet.java
@@ -1,20 +1,38 @@
 package com.freshspire.api.login;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
 
 /**
  * Created by aadisriram on 2/7/16.
  */
-public class CreateUserServlet extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+@RestController
+@RequestMapping("/createuser")
+public class CreateUserServlet {
 
-    }
+    @RequestMapping(value = "/{authId}", method = RequestMethod.GET, produces = "application/json")
+    public String hello(@PathVariable String authId) throws Exception {
+        URL fbAuth = new URL("https://graph.facebook.com/me?access_token=" + authId);
+        URLConnection urlConnection = fbAuth.openConnection();
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(
+                        urlConnection.getInputStream()));
+        StringBuilder inputLineBuffer = new StringBuilder();
+        String inputLine;
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        while ((inputLine = in.readLine()) != null) {
+            inputLineBuffer.append(inputLine);
+        }
 
+        in.close();
+
+        return inputLineBuffer.toString();
     }
 }
--- src/main/java/com/freshspire/api/login/LoginServlet.java
+++ src/main/java/com/freshspire/api/login/LoginServlet.java
@@ -1,20 +1,21 @@
 package com.freshspire.api.login;
 
-import javax.servlet.ServletException;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
 import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 /**
  * Created by aadisriram on 2/7/16.
  */
+@RestController
+@RequestMapping("/login")
 public class LoginServlet extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-    }
-
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.getWriter().println("codedeployed BOOOOOOOM BOOM");
+    @RequestMapping(method = RequestMethod.GET)
+    public String login() {
+        String result= "This works! Welcome to our RESTful service";
+        return result;
     }
 }
