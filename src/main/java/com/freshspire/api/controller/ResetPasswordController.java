7db67907e199aafb1ac4a0486a22996eee83caff
Mitch Rees-Jones <reesjones@users.noreply.github.com>
2016-02-14 00:27:39 -0500

    Add controllers for /users/* endpoints

--- src/main/java/com/freshspire/api/controller/ResetPasswordController.java
+++ src/main/java/com/freshspire/api/controller/ResetPasswordController.java
@@ -4,21 +4,17 @@ import com.freshspire.api.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by reesjones on 2/13/16.
  */
 @RestController
-@RequestMapping("/users/login")
-public class LoginController {
-
-    @Autowired
-    private UserService userService;
+@RequestMapping("/users/reset-password")
+public class ResetPasswordController {
 
     @RequestMapping(method = RequestMethod.GET)
-    public String login() {
-        return "You're at the /users/login controller";
+    public String getResetPassword() {
+        return "You're at the /users/reset-password controller";
     }
 }
