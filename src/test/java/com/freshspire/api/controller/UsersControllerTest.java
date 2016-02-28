72c9b201ea6f55e5da373359f53db88d69de6686
mwreesjo <mwreesjo@ncsu.edu>
2016-02-28 17:35:31 -0500

    Add failing test case to ensure run config is running tests

--- src/test/java/com/freshspire/api/controller/UsersControllerTest.java
+++ src/test/java/com/freshspire/api/controller/UsersControllerTest.java
@@ -1,9 +1,19 @@
 package com.freshspire.api.controller;
 
+import org.junit.Test;
+
 /**
  * com.freshspire.api.controller
  *
  * @created 2/28/16.
  */
 public class UsersControllerTest {
+
+    /**
+     * I'm using this test case to make sure run config is running the test suite
+     */
+    @Test
+    public void thisShouldFail() {
+        assert(false);
+    }
 }
