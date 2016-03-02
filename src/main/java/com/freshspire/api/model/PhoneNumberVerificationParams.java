73e85a2c6f2ccf158e476f73d7565eb2a7809f3a
mwreesjo <mwreesjo@ncsu.edu>
2016-03-01 22:30:51 -0500

    Add newPassword to PhoneNumberVerificationParams

--- src/main/java/com/freshspire/api/model/PhoneNumberVerificationParams.java
+++ src/main/java/com/freshspire/api/model/PhoneNumberVerificationParams.java
@@ -9,6 +9,8 @@ public class PhoneNumberVerificationParams {
 
     private String verificationCode;
 
+    private String newPassword;
+
     public String getPhoneNumber() {
         return phoneNumber;
     }
@@ -23,5 +25,13 @@ public class PhoneNumberVerificationParams {
 
     public void setVerificationCode(String verificationCode) {
         this.verificationCode = verificationCode;
+    }
+
+    public String getNewPassword() {
+        return newPassword;
+    }
+
+    public void setNewPassword(String newPassword) {
+        this.newPassword = newPassword;
     }
 }
