2aa34c7e3148534bd190adff0b43dc4a90338122
mwreesjo <mwreesjo@ncsu.edu>
2016-03-12 00:06:30 -0500

    Add user service utility methods

--- src/main/java/com/freshspire/api/service/UserService.java
+++ src/main/java/com/freshspire/api/service/UserService.java
@@ -10,5 +10,7 @@ public interface UserService {
     void deleteUser(String userId, String authKey);
     boolean authenticateUser(String userId, String authKey);
     User getUserByPhoneNumber(String phoneNumber);
+    boolean userExistsWithPhoneNumber(String phoneNumber);
     User getUserByApiKey(String apiKey);
+    boolean userExistsWithApiKey(String apiKey);
 }
