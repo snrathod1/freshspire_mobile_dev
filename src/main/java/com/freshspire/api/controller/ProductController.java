f49f1a1707f3adecf0269533807fd3edb0dd9ed5
Xiaohang Miao <xmiao2@ncsu.edu>
2016-09-24 02:26:09 -0400

    Added ability to handle thumbnail downloading

--- src/main/java/com/freshspire/api/controller/ProductController.java
+++ src/main/java/com/freshspire/api/controller/ProductController.java
@@ -7,7 +7,11 @@ import com.freshspire.api.service.UserService;
 import com.freshspire.api.utils.ResponseUtil;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResourceLoader;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -17,6 +21,8 @@ import java.io.IOException;
 @RestController
 @RequestMapping("/products")
 public class ProductController {
+    private static final String PRODUCT_NOT_FOUND_TEMPLATE = "Product with ID %d not found";
+    private static final String PRODUCT_THUMBNAIL_NOT_FOUND_TEMPLATE = "Product thumbnail with Product ID %d not found";
 
     private ProductService productService;
     private UserService userService;
@@ -77,10 +83,29 @@ public class ProductController {
         Product product = productService.getProductById(productId);
 
         if(product == null) {
-            return ResponseUtil.notFound("Product with ID " + productId + " not found");
+            return ResponseUtil.notFound(String.format(PRODUCT_NOT_FOUND_TEMPLATE, productId));
         } else {
             return ResponseUtil.makeProductObjectResponse(product, HttpStatus.OK);
         }
     }
 
+    @RequestMapping(value = "thumbnails/{productId}", method = RequestMethod.GET, produces = "application/json")
+    public ResponseEntity getProductThumbnailById(@PathVariable int productId, @RequestHeader("Authorization") String apiKey) {
+        if(userService.getUserByApiKey(apiKey) == null) {
+            return ResponseUtil.unauthorized("Unauthenticated");
+        }
+        Product product = productService.getProductById(productId);
+
+        if(product == null) {
+            return ResponseUtil.notFound(String.format(PRODUCT_NOT_FOUND_TEMPLATE, productId));
+        }
+
+        Resource resource = new FileSystemResourceLoader().getResource(product.getThumbnail());
+
+        if(!resource.exists()) {
+            return ResponseUtil.notFound(String.format(PRODUCT_THUMBNAIL_NOT_FOUND_TEMPLATE, productId));
+        }
+
+        return ResponseEntity.ok().header(HttpHeaders.CONTENT_TYPE, MediaType.IMAGE_JPEG_VALUE).body(resource);
+    }
 }
